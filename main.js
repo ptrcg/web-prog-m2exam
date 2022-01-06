4c88582cdf65f2b63fbcd323cf62ad61ff763312
Whytel Macro <daPiang@mcm.edu.ph>
2022-01-07 00:17:13 +0800

    added no matches

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 let sidebar = document.getElementById('side');
 
 function heartFunction() {
-    sidebar.innerHTML += '<div class="messages"><div class="avatar"><img src='+card.src+' alt=""></div><div class="friend"><div class="user">'+username.innerHTML+'</div><div class="text">Say hello!</div></div></div>';
-    unmatchFunction();
+    if(arrState!=10) {
+        sidebar.innerHTML += '<div class="messages"><div class="avatar"><img src='+card.src+' alt=""></div><div class="friend"><div class="user">'+username.innerHTML+'</div><div class="text">Say hello!</div></div></div>';
+        unmatchFunction();
+    }
+    
 }
 
-let pic_arr = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg','img/6.jpg','img/7.png','img/8.jpg','img/9.jpg','img/10.jpg',];
+let pic_arr = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg','img/6.jpg','img/7.png','img/8.jpg','img/9.jpg','img/10.jpg','img/none.png'];
 
-let name_arr = ['Chico','Lara','Tasha','Tyler','Alex','Trisha','Rocco','Henry','Ashley','Fred'];
+let name_arr = ['Chico','Lara','Tasha','Tyler','Alex','Trisha','Rocco','Henry','Ashley','Fred',''];
 
-let age_arr = [', 8',', 6',', 9',', 7',', 12',', 5',', 10',', 17',', 21',', 7'];
+let age_arr = [', 8',', 6',', 9',', 7',', 12',', 5',', 10',', 17',', 21',', 7',''];
 
-let distance_arr = ['12 kilometers away','30.1 kilometers away','6.7 kilometers away','112 kilometers away','89 kilometers away','4 kilometers away','1.7 kilometers away','99.2 kilometers away','32 kilometers away','69 kilometers away'];
+let distance_arr = ['12 kilometers away','30.1 kilometers away','6.7 kilometers away','112 kilometers away','89 kilometers away','4 kilometers away','1.7 kilometers away','99.2 kilometers away','32 kilometers away','69 kilometers away',''];
 
 let card = document.getElementById('user');
 let username = document.getElementById('name');
@@ -21,10 +24,15 @@ let distance = document.getElementById('distance');
 let arrState = -1;
 
 function unmatchFunction() {
-    if(arrState==9){arrState=-1}
-    arrState+=1;
-    card.src=pic_arr[arrState];
-    username.innerHTML=name_arr[arrState];
-    age.innerHTML=age_arr[arrState];
-    distance.innerHTML=distance_arr[arrState];
+    if(arrState!=10){
+        arrState+=1;
+        card.src=pic_arr[arrState];
+        username.innerHTML=name_arr[arrState];
+        age.innerHTML=age_arr[arrState];
+        distance.innerHTML=distance_arr[arrState];
+    }
 }
+
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let button3 = document.getElementById('button3');
